73343d6c4fd929a6e947f19dc9f7a1d41f27b0cf
Russel <johnrusselpunio@gmail.com>
2021-12-08 21:52:16 +0800

    add content and styling to all components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "Just a test recipe", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQRy5bNnoekjnXVU3TiXLd8xcXkCiZ3EcBSw&usqp=CAU"),
+    new Recipe("Test Recipe", "Just a test recipe", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQRy5bNnoekjnXVU3TiXLd8xcXkCiZ3EcBSw&usqp=CAU")
+  ];
   constructor() { }
 
   ngOnInit(): void {
